4f01a89126ae1fe77bb0ccaef23f4982773b6c7e
mytysoldier <yoshi008t@gmail.com>
2023-07-08 12:35:00 +0900

    delivery notify

--- delivery_notify/app.js
+++ delivery_notify/app.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    const threads = GmailApp.search('subject:(配達完了:ご注文商品の配達が完了しました。)');
+    const threads = GmailApp.search('subject:(お荷物お届け完了のお知らせ)');
     let unreadMailExists = false;
     // 未読メッセージがあるかチェック
     for (thread of threads) {
@@ -11,25 +11,22 @@ function myFunction() {
         // Slackで配達完了したことを通知
         sendSlack("荷物届いたよ！");
     }
-    // Logger.log(threads[0].isUnread());
-    // Logger.log(threads[0].getMessages()[0].getBody());
 }
 
 function sendSlack(slackText) {
-    var userProperties = PropertiesService.getScriptProperties();
-    var api_key = userProperties.getProperty('webhooks_APIKEY');
-    var webHookUrl = api_key;
+    const userProperties = PropertiesService.getScriptProperties();
+    const webhookUrl = userProperties.getProperty('webhook_url');
 
-    var jsonData =
+    const jsonData =
     {
         "text": slackText,
         "link_names": 1,
-        "username": "配達完了"
+        "username": "荷物配達完了"
     };
 
-    var payload = JSON.stringify(jsonData);
+    const payload = JSON.stringify(jsonData);
 
-    var options =
+    const options =
     {
         "method": "post",
         "contentType": "application/json",
@@ -37,5 +34,5 @@ function sendSlack(slackText) {
     };
 
     // リクエスト
-    UrlFetchApp.fetch(webHookUrl, options);
+    UrlFetchApp.fetch(webhookUrl, options);
 }
